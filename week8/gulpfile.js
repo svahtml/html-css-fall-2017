4673799a7846ab6f561dc5cd4440e5d8ebabcb85
Dan Hahn <dan@svahtml.com>
2017-11-13 21:27:05 -0500

    Final week 8 files

--- week8/gulpfile.js
+++ week8/gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
